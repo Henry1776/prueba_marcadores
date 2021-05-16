d0ea79979a7b1aa24171ef25c15184c65da0ee08
henry <henryartavia767@hotmail.com>
2021-05-15 20:42:29 -0600

    modified the bookmark index and seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,50 @@ Bookmark.destroy_all
 Category.destroy_all
 Kind.destroy_all
 
+bands = {
+  yes: 'https://es.wikipedia.org/wiki/Yes_(banda)',
+  gojira: 'https://www.gojira-music.com/',
+  death: 'https://en.wikipedia.org/wiki/Death_(metal_band)',
+  primus: 'https://en.wikipedia.org/wiki/Primus_(band)',
+  porcupine:  'https://en.wikipedia.org/wiki/Porcupine_Tree'
+}
 
-c = Category.create!(name: 'Progresivo')
-k = Kind.create!(name: 'Musica')
+movies = {
+  "Stars Wars": 'https://es.wikipedia.org/wiki/Yes_(banda)',
+  "The hining": 'https://www.gojira-music.com/',
+  "Born of 4th of july": 'https://en.wikipedia.org/wiki/Death_(metal_band)',
+  "Back to the future": 'tps://en.wikipedia.org/wiki/Primus_(band)',
+  "It":  'https://en.wikipedia.org/wiki/Porcupine_Tree'
+}
 
-7.times do |i|
-  Bookmark.create!(kind_id:k.id, category_id:c.id, title: 'Yes', url: 'https://es.wikipedia.org/wiki/Yes_(banda)' )
+philosofy = {
+  "Platon": 'https://es.wikipedia.org/wiki/Yes_(banda)',
+  "Aristoteles": 'https://www.gojira-music.com/',
+  "Socrates": 'https://en.wikipedia.org/wiki/Death_(metal_band)',
+  "Seneca": 'tps://en.wikipedia.org/wiki/Primus_(band)',
+  "Kant":  'https://en.wikipedia.org/wiki/Porcupine_Tree'
+}
+
+category = Category.create!(name: 'Progresivo')
+kind = Kind.create!(name: 'Musica')
+
+bands.each do |k, v|
+  Bookmark.create!(kind_id: kind.id, category_id: category.id, title: k.to_s, url: v )
+  puts "++++++++++++++++++++++++++++++++++++++++++++++++"
+end
+
+category = Category.create!(name: 'Cine')
+kind = Kind.create!(name: 'Peliculas')
+
+movies.each do |k, v |
+  Bookmark.create!(kind_id: kind.id, category_id: category.id, title: k, url: v )
+  puts "++++++++++++++++++++++++++++++++++++++++++++++++"
+end
+
+category = Category.create!(name: 'Filosofia')
+kind = Kind.create!(name: 'Literatura')
+
+philosofy.each do |k, v|
+  Bookmark.create!(kind_id: kind.id, category_id: category.id, title: k, url: v )
   puts "++++++++++++++++++++++++++++++++++++++++++++++++"
 end
